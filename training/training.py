7e5fb18fa6adcf6f4a322d067f4df45cd0dea8d0
Owen Jaques <owenjaques2000@gmail.com>
2021-04-04 17:34:14 -0700

    added support to start games

--- training/training.py
+++ training/training.py
@@ -11,21 +11,38 @@ def start_snakes(num_snakes, snakes):
 		port = '808' + str(i)
 		snakes.append(
 			{
-				'url': 'https://localhost:' + port,
+				'url': 'http://localhost:' + port,
 				'process': Process(target=start_server, args=(port,))
 			}
 		)
 		snakes[i]['process'].start()
 
 def kill_snakes(num_snakes, snakes):
+	# kills snakes running on seperate processes
 	for i in range(num_snakes):
 		kill(snakes[i]['process'].pid, SIGTERM)
 		snakes[i]['process'].join(3)
 
+def start_game(num_snakes, snakes):
+	# creates a game with snakes running on seperate processes
+	cmd = './battlesnake play -W 11 -H 11'
+	for i in range(num_snakes):
+		cmd = f"{cmd} --name snake{str(i)} --url {snakes[i]['url']}"
+		
+	game = sp.run(cmd, shell=True, capture_output=True, cwd='training/engine')
+
 if __name__ == '__main__':
 	snakes = []
-	num_snakes = 2
+	num_snakes = 3
 
 	start_snakes(num_snakes, snakes)
-	sleep(5)
+
+	for i in range(5, 0, -1):
+		sleep(1)
+		print(f'Starting game in: {str(i)}')
+
+	start_game(num_snakes, snakes)
+
+	sleep(10)
+
 	kill_snakes(num_snakes, snakes)
