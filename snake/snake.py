f734ce5c0d7963af5a893b07587fc8ad872e792e
Owen Jaques <owenjaques2000@gmail.com>
2021-04-04 17:55:21 -0700

    cleaned up snake.py

--- snake/snake.py
+++ snake/snake.py
@@ -2,20 +2,24 @@ from snake.utilities import *
 import random
 
 class Snake:
-	def __init__(self):
-		self.game_saver = GameSaver()
+	def __init__(self, save_game=False):
+		self.save_game = save_game
+		self.game_saver = GameSaver() if save_game else None
 
 	def start(self, data):
 		pass
 
 	def move(self, data):
-		self.game_saver.save_move(data)
-		print_map(get_map(data))
+		if self.save_game:
+			self.game_saver.save_move(data)
+		
 		return self.get_move(data)
 
 	def end(self, data):
-		self.game_saver.save_move(data)
-		self.game_saver.save_game_to_file()
+		if self.save_game:
+			self.game_saver.save_move(data)
+			self.game_saver.save_game_to_file()
+		
 		print('Winning Game?', not did_die(data))
 
 	def get_move(self, data):
