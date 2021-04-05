f0d076c6b2b75d86b416c2d9bc354b09e350cdc1
Owen Jaques <owenjaques2000@gmail.com>
2021-04-04 17:52:38 -0700

    moved to a simpler one snake model which will have the snake compete against itself

--- training/training.py
+++ training/training.py
@@ -5,44 +5,33 @@ from time import sleep
 from os import kill
 from signal import SIGTERM
 
-def start_snakes(num_snakes, snakes):
-	# creates a seperate process for every snake created
-	for i in range(num_snakes):
-		port = '808' + str(i)
-		snakes.append(
-			{
-				'url': 'http://localhost:' + port,
-				'process': Process(target=start_server, args=(port,))
-			}
-		)
-		snakes[i]['process'].start()
+def start_snake(port):
+	process = Process(target=start_server, args=(port,))
+	process.start()
+	return process
 
-def kill_snakes(num_snakes, snakes):
-	# kills snakes running on seperate processes
-	for i in range(num_snakes):
-		kill(snakes[i]['process'].pid, SIGTERM)
-		snakes[i]['process'].join(3)
+def kill_snake(process):
+	kill(process.pid, SIGTERM)
+	process.join(3)
 
-def start_game(num_snakes, snakes):
-	# creates a game with snakes running on seperate processes
+def start_game(num_snakes, snake_url):
+	# creates a game with 'num_snakes' amount of the same snake
 	cmd = './battlesnake play -W 11 -H 11'
 	for i in range(num_snakes):
-		cmd = f"{cmd} --name snake{str(i)} --url {snakes[i]['url']}"
+		cmd = f"{cmd} --name snake{str(i)} --url {snake_url}"
 		
 	game = sp.run(cmd, shell=True, capture_output=True, cwd='training/engine')
 
 if __name__ == '__main__':
-	snakes = []
-	num_snakes = 3
-
-	start_snakes(num_snakes, snakes)
-
+	num_snakes = 2
+	port = '8080'
+	url = 'http://0.0.0.0:' + port
+	
+	snake = start_snake(port)
 	for i in range(5, 0, -1):
 		sleep(1)
 		print(f'Starting game in: {str(i)}')
 
-	start_game(num_snakes, snakes)
-
+	start_game(num_snakes, url)
 	sleep(10)
-
-	kill_snakes(num_snakes, snakes)
+	kill_snake(snake)
